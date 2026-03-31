216325cb28b7760d0d8cb6f08406e4677916fd1c
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2026-03-31 17:06:12 +0530

    refactor: update UsernameResendclass.java

--- lib/org/gluu/agama/usernameclass/UsernameResendclass.java
+++ lib/org/gluu/agama/usernameclass/UsernameResendclass.java
@@ -15,7 +15,11 @@ public abstract class UsernameResendclass {
 
     public abstract boolean validateOTPCode(String phone, String code);
 
-    
+    // No-arg (used by Agama engine internally via reflection)
+    public static UsernameResendclass getInstance() {
+        return new JansForgetUsername();
+    }
+
     public static UsernameResendclass getInstance(HashMap config) {
         return new JansForgetUsername(config);
     }
